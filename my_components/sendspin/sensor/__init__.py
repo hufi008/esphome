d902173f53c186bc2569d78d53880c4e5a6c5190
hufi008 <thorsten@hufschmidt.de>
2026-09-13 22:34:31 +0200

    Spectrum Visualizer eingebaut

--- my_components/sendspin/sensor/__init__.py
+++ my_components/sendspin/sensor/__init__.py
@@ -9,8 +9,20 @@ from esphome.const import (
     UNIT_MILLISECOND,
 )
 from esphome.types import ConfigType
+from esphome import automation
 
-from .. import CONF_SENDSPIN_ID, SendspinHub, request_metadata_support, sendspin_ns
+from .. import (
+    CONF_SENDSPIN_ID,
+    SendspinHub,
+    request_metadata_support,
+    request_visualizer_support,
+    register_visualizer_config,
+    sendspin_ns,
+    VisualizerDataType,
+    VISUALIZER_DATA_SPECTRUM,
+    CONF_N_DISP_BINS,
+    CONF_RATE_MAX,
+)
 
 CODEOWNERS = ["@kahrendt"]
 DEPENDENCIES = ["sendspin"]
@@ -18,6 +30,9 @@ DEPENDENCIES = ["sendspin"]
 CONF_TRACK = "track"
 CONF_TRACK_PROGRESS = "track_progress"
 CONF_TRACK_DURATION = "track_duration"
+CONF_SPECTRUM = "spectrum"
+CONF_ON_SPECTRUM = "on_spectrum"
+
 
 SendspinTrackProgressSensor = sendspin_ns.class_(
     "SendspinTrackProgressSensor",
@@ -39,13 +54,23 @@ _METADATA_TYPE_ENUM = {
     CONF_TRACK: SendspinNumericMetadataTypes.TRACK,
 }
 
+# Hufi
+SendspinSpectrum = sendspin_ns.class_(
+    "SendspinSpectrum",
+    cg.Component,
+)
+# ifuH
 
+
+# Hufi
 def _request_roles(config: ConfigType) -> ConfigType:
     """Request the necessary Sendspin roles for the sensor."""
-    request_metadata_support()
-
+    if config[CONF_TYPE] == CONF_SPECTRUM:
+        request_visualizer_support()
+    else:
+        request_metadata_support()
     return config
-
+# ifuH
 
 _HUB_ID_SCHEMA = cv.Schema({cv.GenerateID(CONF_SENDSPIN_ID): cv.use_id(SendspinHub)})
 
@@ -62,6 +87,14 @@ def _metadata_schema(**sensor_kwargs):
         .extend(cv.COMPONENT_SCHEMA)
     )
 
+# Hufi
+def _register(config: ConfigType) -> ConfigType:
+    if config[CONF_TYPE] == CONF_SPECTRUM:
+        register_visualizer_config(config)
+    else:
+        request_metadata_support()
+    return config
+# ifuH
 
 CONFIG_SCHEMA = cv.All(
     cv.typed_schema(
@@ -80,19 +113,47 @@ CONFIG_SCHEMA = cv.All(
             ),
             CONF_YEAR: _metadata_schema(),
             CONF_TRACK: _metadata_schema(),
+            # Hufi
+            CONF_SPECTRUM: cv.Schema({
+                cv.GenerateID(): cv.declare_id(SendspinSpectrum),
+
+                cv.Optional(CONF_RATE_MAX, default="10Hz"): cv.All(
+                    cv.frequency,
+                    cv.float_range(min=1.0, max=60.0),
+                ),
+
+                cv.Optional(CONF_N_DISP_BINS, default=32): cv.int_range(min=1, max=255),
+
+                cv.Optional(CONF_ON_SPECTRUM): automation.validate_automation({}),
+            }).extend(_HUB_ID_SCHEMA).extend(cv.COMPONENT_SCHEMA),
+            # ifuH
         },
         key=CONF_TYPE,
     ),
     cv.only_on_esp32,
-    _request_roles,
+    _register,
 )
 
 
+# Hufi
 async def to_code(config: ConfigType) -> None:
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
     await cg.register_parented(var, config[CONF_SENDSPIN_ID])
-    await sensor.register_sensor(var, config)
 
+    if config[CONF_TYPE] != CONF_SPECTRUM:
+        await sensor.register_sensor(var, config)
+    if config[CONF_TYPE] == CONF_SPECTRUM:
+        parent = await cg.get_variable(config[CONF_SENDSPIN_ID])
+
+        for conf in config.get(CONF_ON_SPECTRUM, []):
+            await automation.build_callback_automation(
+                var,
+                "add_on_spectrum_callback",
+                [(cg.std_vector.template(cg.uint16), "x")],
+                conf,
+            )
+            
     if (metadata_type := _METADATA_TYPE_ENUM.get(config[CONF_TYPE])) is not None:
         cg.add(var.set_metadata_type(metadata_type))
+# ifuH
